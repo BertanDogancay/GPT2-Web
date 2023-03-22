ada617d8b51ee48c5683b71c1ea4050388d36b61
Bertan Dogancay <111835151+BertanDogancay@users.noreply.github.com>
2023-03-22 03:47:57 -0400

    Update app.py

--- GPT2_Web/src/app.py
+++ GPT2_Web/src/app.py
@@ -17,8 +17,8 @@ def get_bot_response():
     
     for step in range(1):
         new_user_input_ids = tokenizer.encode(userText + tokenizer.eos_token, return_tensors='pt')
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids 
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
+        chat_history_ids = model.generate(bot_input_ids, max_length=1000,pad_token_id=tokenizer.eos_token_id, no_repeat_ngram_size=3, do_sample=True, top_k=100, top_p=0.7,temperature = 0.8)
         return str(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True))
  
  
